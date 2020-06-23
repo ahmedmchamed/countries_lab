d53b651c739fde4fff54a1c7c0c654865b2ee463
Ahmed Hamed <tunashark@gmail.com>
2020-06-23 14:41:51 +0100

    individual country select

--- src/app.js
+++ src/app.js
@@ -3,23 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
     new Vue({
         el: '#app',
         data: {
-            countries: [
-            ]
+            countries: [],
+            countryInfo: null
         },
         mounted() {
             fetch("https://restcountries.eu/rest/v2/all")
             .then(response => response.json())
-            .then(data => this.countries = data);
+            .then(data => this.countries = data)
+            .then(list => list.sort(function (a,b) { return a.population - b.population; }))
         },
         computed: {
             totalPopulation: function () {
                 return this.countries.reduce((runningTotal, country) => {
                     return runningTotal + country.population;
                 }, 0)
-            }
+            },
+            filteredCountrys: functio
+
         },
         methods: {
-
+            countrySelected: function (event) {
+                this.countryInfo = event.target.value
+            }
         }
     });
 });
